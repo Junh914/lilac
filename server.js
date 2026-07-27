27a35aa347f593aba6350399210bd55449b8ff45
Junh914 <cheonjunhyeong23@gmail.com>
2026-07-27 23:22:49 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// 정적 파일 제공 (index 파일이 있는 현재 폴더)
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
-io.on('connection', (socket) => {
-    console.log('User has connected');
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User has left');
-    });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-// 렌더 서버가 지정해 주는 포트 번호를 자동으로 사용하도록 설정 (아주 중요!)
+io.on('connection', (socket) => {
+  console.log('유저가 접속했습니다.');
+
+  // 클라이언트로부터 데이터({ nickname, text, time })를 받아 모두에게 방송
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User has left the server');
+  });
+});
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
+http.listen(PORT, () => {
+  console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
